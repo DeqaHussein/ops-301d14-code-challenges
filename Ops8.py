fde8579637fc06df7f678f7f53ddcab67163ca75
Deqa Hussein <147355699+DeqaHussein@users.noreply.github.com>
2023-12-07 00:45:22 -0600

    Create Ops8.py

---
+++ Ops8.py
@@ -0,0 +1,17 @@
+# Create a list of ten string elements
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+
+# Print the fourth element of the list
+print("Fourth element:", my_list[3])
+
+# Print the sixth through tenth element of the list
+print("Sixth through tenth elements:", my_list[5:10])
+
+# Change the value of the seventh element to “onion”
+my_list[6] = "onion"
+
+# Print the updated list
+print("Updated list:", my_list)
+
+
+resource - chatgpt
