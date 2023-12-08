3019d0afea9d42ac3118e47db05ecedec0627a5e
Deqa Hussein <147355699+DeqaHussein@users.noreply.github.com>
2023-12-07 23:35:13 -0600

    Create Ops9.py

---
+++ Ops9.py
@@ -0,0 +1,22 @@
+# Example values
+a = 5
+b = 10
+
+# If statement with equality condition
+if a == b:
+    print("a is equal to b.")
+elif a != b:  # elif with not equal condition
+    print("a is not equal to b.")
+
+# If statement with less than condition
+if a < b:
+    print("a is less than b.")
+
+# If statement with greater than or equal to condition
+if a >= b:
+    print("a is greater than or equal to b.")
+else:  # else statement for when none of the conditions are met
+    print("a is not greater than or equal to b.")
+
+
+resource - chatgpt
